79c1b87fa8cd12870bdb75389b1c4dc62847eabb
Anton Horodchuk <antoman1994@gmail.com>
2022-01-30 21:58:02 +0200

    [WIP] Simplified settings structure

--- robozhab/base/settings.py
+++ robozhab/base/settings.py
@@ -1,6 +1,6 @@
+import datetime
 from functools import lru_cache
 
-import datetime
 from pydantic import BaseSettings
 
 
@@ -37,3 +37,49 @@ class Settings(BaseSettings):
         case_sensitive = False
         env_file = '.env'
         env_file_encoding = 'utf-8'
+
+
+class SettingsV2(BaseSettings):
+    API_ID: int
+    API_HASH: str
+
+    CHAT_ID: int
+    TIMEZONE: int = +3
+
+    WORK_TYPE: int = 2  # Столовая, Крупье, Грабитель
+    FEED_TYPE: int = 2  # Покормить, Откормить
+    HAS_A_CHILD: bool = False
+    HAS_A_CLAN: bool = False
+
+    class Config:
+        case_sensitive = True
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+    @classmethod
+    def from_v1(cls, settings_v1: Settings):
+
+        feed_type = 0
+        if settings_v1.feed_text.lower() == "покормить жабу":
+            feed_type = 1
+        elif settings_v1.feed_text.lower() == "откормить жабу":
+            feed_type = 2
+
+        work_type = 0
+        if settings_v1.work_text.lower() == "поход в столовую":
+            work_type = 1
+        elif settings_v1.work_text.lower() == "работа крупье":
+            work_type = 2
+        elif settings_v1.work_text.lower() == "работа грабитель":
+            work_type = 3
+
+        return SettingsV2(
+            API_ID=settings_v1.api_id,
+            API_HASH=settings_v1.api_hash,
+            CHAT_ID=settings_v1.chat_id,
+            TIMEZONE=settings_v1.timezone,
+            HAS_A_CHILD=settings_v1.has_a_child,
+            HAS_A_CLAN=settings_v1.has_a_clan,
+            WORK_TYPE=work_type,
+            FEED_TYPE=feed_type,
+        )
